8ac17f3f9156b14a71e858d0870a026b0fcca3cd
HankCode <henrik@naesmark.com>
2023-09-28 13:06:07 +0200

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState, useRef } from "react";
+import { AiFillInstagram, AiFillFacebook, AiFillYoutube } from "react-icons/ai";
+import { BsDiscord } from "react-icons/bs";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      console.log("Hello world!");
+    }, 2000);
+    setLoading(false);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <section className="text-center h-screen max-w-[1000px] mx-auto flex items-center justify-center">
+      <div className="px-4 pb-32 md:pb-0">
+        <div className="md:w-[475px] w-[275px] mx-auto">
+          <img src="/np_logo_full_med.png" alt="Nevilles Playground Logotype" />
+        </div>
+        {/* <div className='md:mt-4 mt-3'>
+        <div className='display-1 text-red-500 typewriter'>{quote}</div>
+      </div> */}
+        <div className="md:text-base text-sm mt-4 md:mt-6 text-gray-400">
+          Fill in your e-mail to get news and updates
+        </div>
+        <div className="rounded-full bg-transparent border-2 border-prim1 md:p-2 p-1 flex items-center gap-x-0 w-full mt-1">
+          <input
+            required
+            name="email"
+            type="email"
+            className="w-full bg-transparent bg-opacity-50 rounded-full border border-transparent outline-none py-1 px-3 leading-8 
+          transition-colors duration-200 ease-in-out text-gray-300 text-base placeholder-gray-500 
+          focus:placeholder-transparent focus:ml-5 italic tracking-wide text-center focus:text-left placeholder:text-base"
+            placeholder="Fill in your e-mail to get news and updates about the development"
+            id="email-input"
+            ref={inputRef}
+          />
+
+          <button
+            className="font-semibold font-sofiapro uppercase md:text-[16px] text-[12px] tracking-wider text-white md:px-5 md:py-3 px-3 py-3 
+        bg-gradient-to-b from-prim1 to-prim2 rounded-full whitespace-nowrap hover:text-gray-600 hover:bg-white"
+            onClick={handleSubmit}
+            disabled={loading === true}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Sign me up
+          </button>
+        </div>
+
+        <p className="text-center mx-auto md:mt-6 mt-4 md:text-base text-sm text-gray-400 max-w-3xl md:px-6">
+          Nevilles Playground will be a digital platform and a social community with the mission of
+          helping individuals find meaning and joy in their lives through implementing the teachings
+          of the 20th century mystic Neville Goddard.
+        </p>
+        <div className="flex items-center justify-center gap-x-3 mt-4 md:mt-6">
+          <a href="https://www.instagram.com/nevillesplayground/" target="_blank" rel="noreferrer">
+            <div className="md:w-14 md:h-14 h-10 w-10 flex items-center justify-center bg-white rounded-lg">
+              <AiFillInstagram
+                className="md:text-[45px] text-[30px] hover:text-prim1 cursor-pointer"
+                style={{ color: "#833AB4" }}
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.facebook.com/groups/734730403843670"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="md:w-14 md:h-14 h-10 w-10 flex items-center justify-center bg-white rounded-lg">
+              <AiFillFacebook
+                className="md:text-[45px] text-[30px] hover:text-prim1 cursor-pointer"
+                style={{ color: "#3b5998" }}
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCMi_gSy9_D0G5MoT7cYre8A"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="md:w-14 md:h-14 h-10 w-10 flex items-center justify-center bg-white rounded-lg">
+              <AiFillYoutube
+                className="md:text-[45px] text-[30px] hover:text-prim1 cursor-pointer"
+                style={{ color: "#c4302b" }}
+              />
+            </div>
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* <div className="pt-6 flex items-center mx-auto justify-center text-gray-400">
+        <div className="md:text-lg text-sm">
+          If you wan't to contribute to this project, or just connect with our growing community,
+          come and chat with us on our:
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <button className="px-4 py-2 bg-white mx-auto mt-5 rounded-lg flex items-center gap-x-2">
+        <BsDiscord className="md:text-4xl text-3xl text-indigo-500 mx-auto" />
+        <a href="https://discord.gg/m2TYcGftxS" target="_blank" rel="noreferrer">
+          <div className="md:text-lg text-base whitespace-nowrap font-semibold font-sofiapro text-indigo-500 uppercase">
+            Discord Server
+          </div>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </button> */}
+    </section>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+      custom: "12rem",
+    },
+    letterSpacing: {
+      ultrawide: ".35em",
+      tighter: "-0.05em",
+      tight: "-0.025em",
+      normal: "0em",
+      wide: "0.025em",
+      wider: "0.05em",
+      widest: "0.1em",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        prim1: "#FF840E",
+        prim2: "#FFBA75",
+        secondary: "#9fb469",
+        third: "#a67c52",
+        bgdark: "#333333",
       },
+      animation: {
+        heropulse: "pulse 3s linear 3",
+      },
+      keyframes: {},
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
